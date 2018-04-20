0451f34fcdbae418611c5a54a95a9203d6c822af
PandarkMeow <marbasdu10@gmail.com>
2018-04-20 17:06:40 +0200

    login

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import {IonicPage, NavController, NavParams, Platform} from 'ionic-angular';
 
+
+declare var window: any;
 /**
  * Generated class for the LoginPage page.
  *
@@ -15,11 +17,45 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class LoginPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, private plateform: Platform) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad LoginPage');
   }
 
+  public login() {
+    this.plateform.ready().then(()=> {
+      this.spotifyLogin().then(success => {
+        alert(success.access_token);
+      }, (error) => {
+        alert(error);
+      });
+    });
+  }
+
+  public spotifyLogin(): Promise<any> {
+    return new Promise(function(resolve, reject) {
+      var browserRef = window.cordova.InAppBrowser.open("https://accounts.spotify.com/authorize?client_id=24db18af2b81458f862fa8e3981b1543&redirect_uri=http://localhost/callback&response_type=token&scope=email", "_blank", "location=no,clearsessioncache=yes,clearcache=yes");
+      browserRef.addEventListener("loadstart", (event) => {
+        if ((event.url).indexOf("http://localhost/callback") === 0) {
+          browserRef.removeEventListener("exit", (event) => {});
+          browserRef.close();
+          var responseParameters = ((event.url).split("#")[1]).split("&");
+          var parsedResponse = {};
+          for (var i = 0; i < responseParameters.length; i++) {
+            parsedResponse[responseParameters[i].split("=")[0]] = responseParameters[i].split("=")[1];
+          }
+          if (parsedResponse["access_token"] !== undefined && parsedResponse["access_token"] !== null) {
+            resolve(parsedResponse);
+          } else {
+            reject("Problem authenticating with Spotify");
+          }
+        }
+      });
+      browserRef.addEventListener("exit", function(event) {
+        reject("The Spotify sign in flow was canceled");
+      });
+    });
+  }
 }
